0180cfd5688daa1a58fc519084798e6f54222c9c
EthanC26 <ethan.cullen@torontofilmschool.ca>
2025-06-05 03:50:19 -0400

    lab 7 incomplete

--- Assets/_Scripts/LoadSaveManager.cs
+++ Assets/_Scripts/LoadSaveManager.cs
@@ -1,8 +1,10 @@
 using System.Xml.Serialization;
+using NUnit.Framework;
 using UnityEngine;
 
 public class LoadSaveManager : MonoBehaviour
 {
+    // Save game data
     [XmlRoot("GameStateData")]
     public class GameStateData
     {
@@ -18,34 +20,41 @@ public class LoadSaveManager : MonoBehaviour
             public float scaleY;
             public float scaleZ;
         }
-
+        // Data for player
         public class DataPlayer
         {
+            //Transform Data
             public DataTransform transformData;
-
+            //Has Collected Gun 01?
             public bool collectedWeapon;
-
+            //Health
             public int health;
         }
-
+        // Data for enemy
         public class DataEnemy
         {
+            //Enemy Transform Data
             public DataTransform transformData;
-
+            // Is Enemy Dead?
             public bool isDead;
-
+            //Health
             public int health;
         }
 
+        // Instance variables
         public DataPlayer playerData = new DataPlayer();
         public DataEnemy enemyData = new DataEnemy();
     }
-
+    // Game data to save/load
     public GameStateData gameStateData = new GameStateData();
-
+    // Saves game data to XML file
     public void Save()
-    { }
-
+    {
+        // Save game data
+    }
+    // Load game data from XML file
     public void Load()
-    { }
+    {
+        //load game data
+    }
 }
